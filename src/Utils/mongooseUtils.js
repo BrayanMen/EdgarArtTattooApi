da0cc709dede9dfbea772fc4caee25400f72b850
Brayan Mendoza <95663591+BrayanMen@users.noreply.github.com>
2025-01-28 12:08:10 +0000

    fixeds

--- src/Utils/mongooseUtils.js
+++ src/Utils/mongooseUtils.js
@@ -1,12 +1,11 @@
 const mongoose = require('mongoose');
-// Configuración común para esquemas
+
 const commonSchemaOptions = {
     timestamps: true,
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
   };
   
-  // Validaciones comunes
   const stringRequired = (fieldName, maxLength = 255) => ({
     type: String,
     required: [true, `${fieldName} es requerido.`],
