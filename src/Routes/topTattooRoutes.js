22d8391698d29608e3f4c3793807f66870913a1e
Brayan Mendoza <95663591+BrayanMen@users.noreply.github.com>
2025-04-23 20:39:44 -0300

    Update topTattooRoutes.js

--- src/Routes/topTattooRoutes.js
+++ src/Routes/topTattooRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { addImageAtPosition,toggleActiveImage } = require('../Controllers/topTattooController');
+const { addImageAtPosition,toggleActiveImage, reOrder } = require('../Controllers/topTattooController');
 
 router.post('/', addImageAtPosition);
 router.patch("/toggle-active/:id", toggleActiveImage);
+router.patch("/reorder", reOrder);
 
 module.exports = router;
